e092dba1e0343c3c2f637fff1ea96e4b75bd2018
kozlov <kozlov@goscomsoft.ru>
2015-08-18 09:58:53 +0700

    upd ProductControllerTest

--- src/test/java/org/rezistenz/product/directory/web/ProductControllerTest.java
+++ src/test/java/org/rezistenz/product/directory/web/ProductControllerTest.java
@@ -1,6 +1,9 @@
 package org.rezistenz.product.directory.web;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +11,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/META-INF/applicationContextWeb.xml"})
@@ -21,8 +26,19 @@ public class ProductControllerTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testList() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+		
+		mockMvc.perform(get("/products"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("products/list"))
+			.andExpect(forwardedUrl("products/list"));
+		
+		mockMvc.perform(get("/products/list"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("products/list"))
+			.andExpect(forwardedUrl("products/list"));
+		
 	}
 
 }
